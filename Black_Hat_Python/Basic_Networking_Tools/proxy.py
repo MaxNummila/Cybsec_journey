43f4e1347d5e32d74462884fcfef7099fd5b4f6b
MNummila <maxnummila@gmail.com>
2026-02-16 17:33:23 +0200

    Refactor proxy_handler and add request/response handlers

--- Black_Hat_Python/Basic_Networking_Tools/proxy.py
+++ Black_Hat_Python/Basic_Networking_Tools/proxy.py
@@ -36,7 +36,59 @@ def hexdump(src, length=16, show=True):
             print(line)
     else:
         return results
-    
+
+
+# Main bulk of proxy logic. Starts by connecting to remote host 
+def proxy_handler(client_socket, remote_host, remote_port, receive_first):
+    remote_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    remote_socket.connect((remote_host, remote_port))
+
+    # Then checks to make sure we dont need to initiate a connection to the remote side and request data 
+    # before going into the main loop. THis is expected by some server daemons.
+    if receive_first:
+        # Uses receive_from function for both sides of the communication. It accepts a connected socket object 
+        # and performs a receive.
+        remote_buffer = receive_from(remote_socket)
+        # Dumps the contents of the packet so that we can inspect it for anything interesting
+        hexdump(remote_buffer)
+
+    # Receives the output and sends the received buffer of response_handler to the local client.
+    remote_buffer = response_handler(remote_buffer)
+    if len(remote_buffer):
+        print("[<==] Sending %d bytes to localhost." % len(remote_buffer))
+        client_socket.send(remote_buffer)
+
+    # Loop to manually read from the local client, process the data, send it to the remote client, 
+    # read from the remote client, process the data, send it to the local client. 
+    # It does this until there is no data detected
+    while True:
+        local_buffer = receive_from(client_socket)
+        if len(local_buffer):
+            line = "[==>] Received %d bytes from localhost." % len(local_buffer)
+            print(line)
+            hexdump(local_buffer)
+
+            local_buffer = request_handler(local_buffer)
+            remote_socket.send(local_buffer)
+            print("[==>] Sent to remote")
+
+        remote_buffer = receive_from(remote_socket)
+        if len(remote_buffer):
+            print("[<==] Received %d bytes from remote." % len(remote_buffer))
+            hexdump(remote_buffer)
+
+            remote_buffer = response_handler(remote_buffer)
+            client_socket.send(remote_buffer)
+            print("[<==] Sent to localhost.")
+
+        # Closes both the local and remote sockets and breaks out of the loop
+        if not len(local_buffer) or not len(remote_buffer):
+            client_socket.close()
+            remote_socket.close()
+            print("[*] No more data. Closing connections.")
+            break
+
+
 # For receiving both local and remote data. Passes in the socket object to be used. Creates an empty byte string called
 # buffer, that will accumulate responses from the socket
 def receive_from(connection):
@@ -54,3 +106,12 @@ def receive_from(connection):
         pass
     # Returns the buffer byte string to the caller, which could be either local or remote machine
     return buffer
+
+# These two can be used to: Modify packet contents, perform fuzzing tasks, test for authentication issues or whatever one wants
+def request_handler(buffer):
+    # perform packet modification
+    return buffer
+
+def response_handler(buffer):
+     # perform packet modifications
+     return buffer
